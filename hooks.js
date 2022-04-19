9ef0192b7de7535aab8b30b4f7f149fdb6477249
Cafe137 <xcafe137@gmail.com>
2022-04-19 14:44:48 +0200

    build: add postMake hook to zip bundle

--- hooks.js
+++ hooks.js
@@ -1,8 +1,29 @@
-const { copySync } = require('fs-extra')
+const { zipSync, unzipSync } = require('cross-zip')
+const { copySync, mkdirSync, existsSync } = require('fs-extra')
 const { join } = require('path')
 
 module.exports = {
-    postPackage: (_, options) => {
-        copySync('static', join(options.outputPaths[0], 'static'))
+    postMake: (_, makeResults) => {
+        const [makeResult] = makeResults
+        const dirName = `${makeResult.packageJSON.name}-${makeResult.platform}-${makeResult.arch}-${makeResult.packageJSON.version}`
+        const name = `${dirName}.zip`
+        if (!existsSync(dirName)) {
+            mkdirSync(dirName)
+        }
+        copySync('static', dirName)
+        if (makeResult.artifacts[0].endsWith('.zip')) {
+            unzipSync(makeResult.artifacts[0], dirName)
+        } else {
+            copySync(makeResult.artifacts[0], dirName)
+        }
+        zipSync(dirName, name)
+        return [
+            {
+                packageJSON: makeResult.packageJSON,
+                artifacts: [join(__dirname, name)],
+                platform: makeResult.platform,
+                arch: makeResult.arch
+            }
+        ]
     }
 }
